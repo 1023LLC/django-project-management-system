09e7aef50ad41f4941623225209b5f98b079bd9d
julius <nyerereoffice@gmail.com>
2024-02-27 05:02:14 +0300

    Add-project feature

--- project/urls.py
+++ project/urls.py
@@ -7,5 +7,6 @@ from . import views
 app_name = 'project'
 
 urlpatterns = [
-    path('', views.projects, name='projects')
+    path('', views.projects, name='projects'),
+    path('add/', views.add_project, name='add')
 ]
--- project/views.py
+++ project/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
 
 from .models import Project
@@ -9,3 +9,18 @@ def projects(request):
     projects = Project.objects.filter(created_by=request.user)
     
     return render(request, 'project/projects.html', {'projects':projects})
+
+
+@login_required
+def add_project(request):
+    if request.method == 'POST':
+        name = request.POST.get('name', '')
+        description = request.POST.get('description', '')
+        
+        if name:
+            Project.objects.create(name=name, description=description, created_by=request.user)
+            
+            return redirect('/projects/')
+        else:
+            print('Not valid!')    
+    return render(request, 'project/add.html')
