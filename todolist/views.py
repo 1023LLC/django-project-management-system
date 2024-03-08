0d1f9f34bd3d36d237b53a82b05da567357bb7d1
julius <nyerereoffice@gmail.com>
2024-03-08 03:44:24 +0300

    Added edit and todolist feature

--- todolist/views.py
+++ todolist/views.py
@@ -7,7 +7,7 @@ from project.models import Project
 from .forms import TodolistForm
 
 
-
+@login_required
 def todolist(request, project_id, pk):
     project = get_object_or_404(Project, pk=project_id, created_by=request.user)
     todolist = get_object_or_404(Todolist, project=project, pk=pk)
@@ -19,7 +19,7 @@ def todolist(request, project_id, pk):
 
 
 
-
+@login_required
 def add(request, project_id):
     project = get_object_or_404(Project, pk=project_id, created_by=request.user)
 
@@ -41,4 +41,35 @@ def add(request, project_id):
         form = TodolistForm()
         
     return render(request, 'todolist/add.html', {'form':form, 'project':project})
+
+
+
+@login_required
+def edit(request, project_id, pk):
+    project = get_object_or_404(Project, pk=project_id, created_by=request.user)
+    todolist = get_object_or_404(Todolist, project=project, pk=pk)
+    
+    
+    if request.method == 'POST':
+        name = request.POST.get('name', '')
+        description = request.POST.get('description', '')
+        
+        if name:
+            todolist.name = name
+            todolist.description = description
+            todolist.save()
             
+            return redirect(f'/projects/{project_id}/{pk}/')    
+    
+    return render(request, 'todolist/edit.html', {'project':project, 'todolist':todolist})
+            
+            
+            
+@login_required
+def delete(request, project_id, pk):
+    project = get_object_or_404(Project, pk=project_id, created_by=request.user)
+    todolist = get_object_or_404(Todolist, project=project, pk=pk)
+    
+    todolist.delete()
+    
+    return redirect(f'/projects/{project_id}/')
